42e17eba0eea1cafe8000bf3dc477d3b3593f8e9
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-30 11:25:45 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-console.log('JavaScript-Number');
+console.log('JavaScript-Number')
 
-const number1 = 3;
-const number2 = 6;
-const number3 = 7;
+const number1 = 3
+const number2 = 6
+const number3 = 7
 
-console.log(`Number1 : ${number1}`);
-console.log(`Number2 : ${number2}`);
-console.log(`Number3 : ${number3}`);
+console.log(`Number1 : ${number1}`)
+console.log(`Number2 : ${number2}`)
+console.log(`Number3 : ${number3}`)
 
-const result1 = number1 + number2;
-console.log(`Number1 + Number2 : ${result1}`);
-const result2 = number2 - number1;
-console.log(`Number2 - Number1 : ${result2}`);
-const result3 = number2 * number3;
-console.log(`Number2 * Number3 : ${result3}`);
-const result4 = number2 / number1;
-console.log(`Number2 / Number1 : ${result4}`);
-const result5 = number3 % number1;
-console.log(`Number3 % Number1 : ${result5}`);
+const result1 = number1 + number2
+console.log(`Number1 + Number2 : ${result1}`)
 
-const string1 = 'Holiday';
-console.log(number1 + string1);
+const result2 = number2 - number1
+console.log(`Number2 - Number1 : ${result2}`)
 
-const array1 = [1, 2, 3];
-console.log(array1);
-array1.push(4, 5);
-console.log(array1);
-array1.pop();
-console.log(array1);
+const result3 = number2 * number3
+console.log(`Number2 * Number3 : ${result3}`)
 
-let array2 = [3, 6, 7];
-let array3 = [7, 34, 54, 7, 8, 4];
+const result4 = number2 / number1
+console.log(`Number2 / Number1 : ${result4}`)
+
+const result5 = number3 % number1
+console.log(`Number3 % Number1 : ${result5}`)
+
+const string1 = 'Holiday'
+console.log(number1 + string1)
+
+// Array operation
+
+const array1 = [1, 2, 3]
+console.log(array1)
+
+array1.push(4, 5)
+console.log(array1)
+
+array1.pop()
+console.log(array1)
+
+const array2 = [3, 6, 7]
+const array3 = [7, 34, 54, 7, 8, 4]
+
+console.log(array2)
+console.log(array3)
